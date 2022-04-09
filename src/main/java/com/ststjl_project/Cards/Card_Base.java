8f1affc6a9cc01dfa5914a56f9af92531f2d1099
UnKinSeong <58870474+UnKinSeong@users.noreply.github.com>
2022-04-09 20:04:17 +0800

    Further Implement variable to CardBase.

--- src/main/java/com/ststjl_project/Cards/Card_Base.java
+++ src/main/java/com/ststjl_project/Cards/Card_Base.java
@@ -15,24 +15,32 @@ public class Card_Base {
         double blood_required,
         double base_damage,
         double base_critical_chance,
-        double base_shield_damage
+        double base_shield_damage,
+        double base_armor_damage
     )
     {
-        this.energy_required = energy_required;
-        this.bone_required = bone_required;
-        this.blood_required = blood_required;
-        this.base_damage = base_damage;
-        this.base_critical_chance = base_critical_chance;
+     this.energy_required = energy_required;
+     this.bone_required = bone_required;
+     this.blood_required = blood_required;
+     this.base_damage = base_damage;
+     this.base_critical_chance = base_critical_chance;
+     this.base_shield_damage = base_shield_damage;
+     this.base_armor_damage = base_armor_damage;
     }
-    public double getEnergy_required(){return this.energy_required;};
-    public double getBone_required(){return this.bone_required;};
-    public double getBlood_required(){return this.blood_required;};
-    public double getBase_damage(){return this.base_damage;};
-    public double getBase_critical_chance(){return this.base_critical_chance;};
+    public double getEnergy_required(){return this.energy_required;}
+    public double getBone_required(){return this.bone_required;}
+    public double getBlood_required(){return this.blood_required;}
+    public double getBase_damage(){return this.base_damage;}
+    public double getBase_critical_chance(){return this.base_critical_chance;}
+    public double getBase_shield_damage(){return this.base_shield_damage;}
+    public double getBase_armor_damage(){return this.base_armor_damage;}
 
-    public void setEnergy_required(double energy_required){this.energy_required = energy_required;};
-    public void setBone_required(double bone_required){this.bone_required = bone_required;};
-    public void setBlood_required(double blood_required){this.blood_required = blood_required;};
-    public void setBase_damage(double base_damage){this.base_damage = base_damage;};
-    public void setBase_critical_chance(double base_critical_chance){this.base_critical_chance = base_critical_chance;};
+    public void setEnergy_required(double energy_required_){this.energy_required = energy_required_;}
+    public void setBone_required(double bone_required_){this.bone_required = bone_required_;}
+    public void setBlood_required(double blood_required_){this.blood_required = blood_required_;}
+    public void setBase_damage(double base_damage_){this.base_damage = base_damage_;}
+    public void setBase_critical_chance(double base_critical_chance_){this.base_critical_chance = base_critical_chance_;}
+    public void setBase_shield_damage(double base_shield_damage_){this.base_shield_damage = base_shield_damage_;}
+    public void setBase_armor_damage(double base_armor_damage_){this.base_armor_damage = base_armor_damage_;}
+
 }
